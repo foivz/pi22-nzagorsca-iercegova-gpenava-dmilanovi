b3e9239f5c761512c57c3b7633340715eab7643e
prok4 <nzagorscak@gmail.com>
2022-06-21 17:25:31 +0200

    Dodana mogućnost dodavanja korisnika.

---
+++ Software/API/PodaciIzBaze/Services/ParkingUsersService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciIzBaze.Services
+{
+    public class ParkingUsersService
+    {
+        public void AddNewUser(TmpUser user)
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                context.TmpUsers.Add(user);
+            }
+        }
+    }
+}
