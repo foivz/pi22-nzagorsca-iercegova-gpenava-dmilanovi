91868e64d4d0c84739440f1ec21cb8a42a59c193
prok4 <nzagorscak@gmail.com>
2022-06-21 17:27:33 +0200

    Dodana mogućnost ispisa svih parkirnih spotova.

---
+++ Software/API/PodaciIzBaze/Services/ParkingSpotService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciIzBaze.Services
+{
+    public class ParkingSpotService
+    {
+        public List<TmpParkingSpot> GetAllParkingSpots()
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                var parkingSpots = context.TmpParkingSpots.ToList();
+                return parkingSpots;
+            }
+        }
+
+    }
+}
