ef7f765d6f65fda13e8a17172c6e3c2c3aa6f45f
prok4 <nzagorscak@gmail.com>
2022-06-21 17:33:13 +0200

    Dodana mogućnost dohvaćanja svih parkirnih senzora

---
+++ Software/API/PodaciIzBaze/Services/ParkingSensorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciIzBaze.Services
+{
+    public class ParkingSensorService
+    {
+        public List<TmpSensor> GetAllParkingSensors()
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                var parkingSensors = context.TmpSensors.ToList();
+                return parkingSensors;
+            }
+        }
+
+    }
+}
