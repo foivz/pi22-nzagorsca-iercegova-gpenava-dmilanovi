ed8e50bab734eb99de2e436e73628271ed16e6fd
prok4 <nzagorscak@gmail.com>
2022-06-21 17:28:15 +0200

    Proširenja mogućnost parking spotova. Dodana mogućnost dohvaćanja određenog parking spota.

--- Software/API/PodaciIzBaze/Services/ParkingSpotService.cs
+++ Software/API/PodaciIzBaze/Services/ParkingSpotService.cs
@@ -16,6 +16,14 @@ namespace PodaciIzBaze.Services
                 return parkingSpots;
             }
         }
+        public List<TmpParkingSpot> GetSpecificParkingSpot(int id)
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                var parkingSpot = context.TmpParkingSpots.Where(x => x.SptParkingSpotId == id).ToList();
+                return (List<TmpParkingSpot>)parkingSpot;
+            }
+        }
 
     }
 }
