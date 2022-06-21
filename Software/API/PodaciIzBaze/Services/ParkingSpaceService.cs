0c7336cdca21f18b113729e5be0d466954a9c0f1
prok4 <nzagorscak@gmail.com>
2022-06-21 17:30:17 +0200

    Dodane mogućnosti dohvaćanja parkirnih spacea.

---
+++ Software/API/PodaciIzBaze/Services/ParkingSpaceService.cs
@@ -0,0 +1,22 @@
+﻿global using PodaciIzBaze.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciIzBaze.Services
+{
+    public class ParkingSpaceService
+    {
+        public List<TmpParkingSpace> GetAllParkingSpaces()
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                var parkingSpaces = context.TmpParkingSpaces.ToList();
+                return parkingSpaces;
+            }
+        }
+
+    }
+}
