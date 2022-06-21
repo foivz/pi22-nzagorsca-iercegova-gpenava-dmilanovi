21385d58c0123b44fdda54be46ee8de8ce2207ac
prok4 <nzagorscak@gmail.com>
2022-06-21 17:20:33 +0200

    Podaci koje smo dobili iz baze podataka. Također imamo klasu pomoću koje pristupamo bazi.

---
+++ Software/API/PodaciIzBaze/Models/TmpParkingSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PodaciIzBaze.Models
+{
+    public partial class TmpParkingSession
+    {
+        public long PssParkingSessionId { get; set; }
+        public long? PssSensorId { get; set; }
+        public long? PssParkingSpotId { get; set; }
+        public DateTimeOffset? PssStartTime { get; set; }
+        public DateTimeOffset? PssEndTime { get; set; }
+    }
+}
---
+++ Software/API/PodaciIzBaze/Models/TmpUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PodaciIzBaze.Models
+{
+    public partial class TmpUser
+    {
+        public int UserId { get; set; }
+        public string Lozinka { get; set; } = null!;
+        public int CardId { get; set; }
+        public string CarTable { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string TipKorisnika { get; set; } = null!;
+    }
+}
