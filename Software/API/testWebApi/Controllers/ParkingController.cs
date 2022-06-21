565f47bbbca7cc9b91cf97bbed173736381c05ec
prok4 <nzagorscak@gmail.com>
2022-06-21 17:41:11 +0200

    ParkingController uz pomoć kojeg dobivamo podatke. Stvoren je konstruktor pomoću kojeg ćemo pristupati podacima iz baze. Također je napravljen HttpGet zahtjev pomoću kojeg dohvaćamo sva parking spaces.

---
+++ Software/API/testWebApi/Controllers/ParkingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PodaciIzBaze.Models;
+using PodaciIzBaze.Services;
+
+namespace testWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParkingController : ControllerBase
+    {
+        private readonly ParkingSpaceService _parkingSpaceService;
+        private readonly ParkingSessionService _parkingSessionService;
+        private readonly ParkingSpotService _parkingSpotService;
+        private readonly ParkingUsersService _parkingUserService;
+        private readonly ParkingSensorService _parkingSensorService;
+        public ParkingController(ParkingSpaceService parkingSpaceService, ParkingUsersService parkingUserService, ParkingSessionService parkingSessionService, ParkingSpotService parkingSpotService, ParkingSensorService parkingSensorService)
+        {
+            _parkingSpaceService = parkingSpaceService;
+            _parkingSessionService = parkingSessionService;
+            _parkingSpotService = parkingSpotService;
+            _parkingUserService = parkingUserService;
+            _parkingSensorService = parkingSensorService;
+        }
+
+        [HttpGet]
+        [Route("allParkingSpaces")]
+        public async Task<ActionResult<List<TmpParkingSpace>>> GetParkingSpaces()
+        {
+            return _parkingSpaceService.GetAllParkingSpaces();
+        }
+
+    }
+}
+
