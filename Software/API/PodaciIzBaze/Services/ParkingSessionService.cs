69b8cfd81e9dafb31fb16b5303880fe80cb68ca4
prok4 <nzagorscak@gmail.com>
2022-06-21 17:31:50 +0200

    Dodana mogućnost dohvaćanja svih parkirnih session

---
+++ Software/API/PodaciIzBaze/Services/ParkingSessionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciIzBaze.Services
+{
+    public class ParkingSessionService
+    {
+        public List<TmpParkingSession> GetAllParkingSessions()
+        {
+            using (var context = new PI2201_DBContext())
+            {
+                var parkingSessions = context.TmpParkingSessions.ToList();
+                return parkingSessions;
+            }
+        }
+
+    }
+}
