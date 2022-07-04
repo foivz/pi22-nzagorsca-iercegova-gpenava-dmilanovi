4a578d5253e34d886fcf52f7709092b089190160
GrgoPenava <96267748+GrgoPenava@users.noreply.github.com>
2022-07-04 20:00:47 +0200

    dodane dozvole za pristup (policy) za pristup apiju u JS (markeri)

--- Software/API/testWebApi/Program.cs
+++ Software/API/testWebApi/Program.cs
@@ -4,7 +4,7 @@ using WebAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +14,17 @@ builder.Services.AddScoped<ParkingSessionService>();
 builder.Services.AddScoped<ParkingSpotService>();
 builder.Services.AddScoped<ParkingUsersService>();
 builder.Services.AddScoped<ParkingSensorService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7236/",
+                                              "https://localhost:7236/api/", "https://localhost:7236/api/Parking/allParkingSpots", "http://127.0.0.1:5500")
+                          .AllowAnyMethod()
+                          .WithHeaders("content-type");
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,9 +35,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
