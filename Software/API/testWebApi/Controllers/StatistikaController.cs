9555275dd8b395ce6b34eb0673374479e17742ed
prok4 <nzagorscak@gmail.com>
2022-07-04 21:30:09 +0200

    Dodane dvije nove rute koje vracaju tip parkirnog mjesta te broj slobodnih mjesta po parkirnirnom spaceu. Vracaju se u obliku stringa te je potrebno razdvojiti podatke, recimo funkcijom split(';');

--- Software/API/testWebApi/Controllers/StatistikaController.cs
+++ Software/API/testWebApi/Controllers/StatistikaController.cs
@@ -49,12 +49,61 @@ namespace WebAPI.Controllers
             return pedesetRandomSenzora;
         }
 
-
         [HttpGet]
         [Route("specificParkingSensor")]
         public async Task<ActionResult<List<TmpSensor>>> GetSpecificParkingSensor(int Id)
         {
             return _parkingSensorService.GetSpecificParkingSensor(Id);
+        }
+
+        [HttpGet]
+        [Route("tipParkirnihMjesta")]
+        public string GetParkingSpots()
+        {
+            var parkirniSpotovi = _parkingSpotService.GetAllParkingSpots();
+            int brojRegular = 0;
+            int brojHendikep = 0;
+            foreach (var item in parkirniSpotovi)
+            {
+                if (item.SptSpotType == "Regular")
+                    brojRegular++;
+                else
+                    brojHendikep++;
+            }
+            string parkirnaMjesta = brojRegular + ";" + brojHendikep;
+            return parkirnaMjesta;
+        }
+
+
+        [HttpGet]
+        [Route("brojSlobodnihMjestaPoParkirnomSpaceu")]
+        public async Task<ActionResult<List<string>>> brojSlobodnihMjestaPoParkirnomSpaceu()
+        {
+            var parkingSpaces = _parkingSpaceService.GetAllParkingSpaces();
+            var parkingSpots = _parkingSpotService.GetAllParkingSpots();
+            List<string> povratniStringSvihPodataka = new List<string>();
+            foreach (var item in parkingSpaces)
+            {
+                List<TmpParkingSpot> parkirnaMjestaParkirnogSpacea = new List<TmpParkingSpot>();
+                int brojOkupiranih = 0;
+                int brojSlobodnih = 0;
+                foreach (var item2 in parkingSpots)
+                {
+                    if (item.PspParkingSpaceId == item2.SptParkingSpaceId)
+                        parkirnaMjestaParkirnogSpacea.Add(item2);
+                }
+                foreach (var item3 in parkirnaMjestaParkirnogSpacea)
+                {
+                    if (item3.SptOccupied == true)
+                        brojOkupiranih++;
+                    else
+                        brojSlobodnih++;
+                }
+                string povratni = "";
+                povratni = item.PspParkingSpaceId + ";" + brojSlobodnih + ";" + brojOkupiranih;
+                povratniStringSvihPodataka.Add(povratni);
+            }
+            return povratniStringSvihPodataka;
         }
     }
 }
