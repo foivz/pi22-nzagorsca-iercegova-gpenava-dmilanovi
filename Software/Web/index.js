2533122bf3c3d4bac3bf20aed20366329f4f5fc6
prok4 <nzagorscak@gmail.com>
2022-07-08 17:58:45 +0200

    Manje promjene u prijavi i registraciji a u indexu je napravljen dropdown te njegovo popunjavanje

--- Software/Web/index.js
+++ Software/Web/index.js
@@ -13,7 +13,7 @@ function initMap() {
   var ukupanBrojMjesta = 0;
   Promise.allSettled([userAction3(), userAction()]).then((podaci3) => {
     $(function () {
-      console.log(podaci3);
+      // console.log(podaci3);
       var parkingSpots = podaci3[1];
       var parkingSessions = podaci3[0];
       var valueParkingSpota = parkingSpots[Object.keys(parkingSpots)[1]];
@@ -31,60 +31,6 @@ function initMap() {
         });
 
         const contentString =
-        '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-        '<div id="bodyContent">' +
-        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-        "sandstone rock formation in the southern part of the " +
-        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-        "south west of the nearest large town, Alice Springs; 450&#160;km " +
-        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-        "Aboriginal people of the area. It has many springs, waterholes, " +
-        "rock caves and ancient paintings. Uluru is listed as a World " +
-        "Heritage Site.</p>" +
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-        "(last visited June 22, 2009).</p>" +
-        "</div>" +
-        "</div>";
-
-        
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-      });
-
-      marker6.addListener("click", () => {
-        infowindow.open({
-          anchor: marker6,
-          map,
-          shouldFocus: false,
-        });
-
-        map.addListener('click', function() {
-          if (infoWindow) infoWindow.close();
-      });
-      });
-
-        for (var j = 0; j < valueParkingSessiona.length; j++) {
-          if (
-            valueParkingSessiona[j].pssParkingSpotId ==
-            valueParkingSpota[i].sptParkingSpotId
-          ) {
-            brojZauzetihMjesta++;
-            brojSlobodnihMjesta--;
-            const marker6 = new google.maps.Marker({
-              position: {
-                lat: valueParkingSpota[i].sptLatitude,
-                lng: valueParkingSpota[i].sptLongitude,
-              },
-              map: map,
-              icon: crvena,
-            });
-            const contentString =
           '<div id="content">' +
           '<div id="siteNotice">' +
           "</div>" +
@@ -106,7 +52,6 @@ function initMap() {
           "</div>" +
           "</div>";
 
-          
         var infowindow = new google.maps.InfoWindow({
           content: contentString,
         });
@@ -118,10 +63,63 @@ function initMap() {
             shouldFocus: false,
           });
 
-          map.addListener('click', function() {
+          map.addListener("click", function () {
             if (infoWindow) infoWindow.close();
+          });
         });
-        });
+
+        for (var j = 0; j < valueParkingSessiona.length; j++) {
+          if (
+            valueParkingSessiona[j].pssParkingSpotId ==
+            valueParkingSpota[i].sptParkingSpotId
+          ) {
+            brojZauzetihMjesta++;
+            brojSlobodnihMjesta--;
+            const marker6 = new google.maps.Marker({
+              position: {
+                lat: valueParkingSpota[i].sptLatitude,
+                lng: valueParkingSpota[i].sptLongitude,
+              },
+              map: map,
+              icon: crvena,
+            });
+            const contentString =
+              '<div id="content">' +
+              '<div id="siteNotice">' +
+              "</div>" +
+              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+              '<div id="bodyContent">' +
+              "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+              "sandstone rock formation in the southern part of the " +
+              "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+              "south west of the nearest large town, Alice Springs; 450&#160;km " +
+              "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+              "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+              "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+              "Aboriginal people of the area. It has many springs, waterholes, " +
+              "rock caves and ancient paintings. Uluru is listed as a World " +
+              "Heritage Site.</p>" +
+              '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+              "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+              "(last visited June 22, 2009).</p>" +
+              "</div>" +
+              "</div>";
+
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString,
+            });
+
+            marker6.addListener("click", () => {
+              infowindow.open({
+                anchor: marker6,
+                map,
+                shouldFocus: false,
+              });
+
+              map.addListener("click", function () {
+                if (infoWindow) infoWindow.close();
+              });
+            });
           }
         }
       }
@@ -178,7 +176,7 @@ const parkingSpaces = async () => {
       },
     }
   );
-  const myJson = await response3.json();
+  const myJson = await response.json();
 
   return myJson;
 };
@@ -187,109 +185,34 @@ function dodajParkingSpaces() {
   Promise.allSettled([parkingSpaces()]).then((parkirniSpaceovi) => {
     $(function () {
       console.log(parkirniSpaceovi);
+      var park = parkirniSpaceovi[0];
+      var valueParkingSpacea = park[Object.keys(park)[1]];
+      var tempList = document.getElementById("parkirniSpaceovi");
+      for (var i = 0; i < valueParkingSpacea.length; i++) {
+        var option = document.createElement("option");
+        option.text = valueParkingSpacea[i].pspLabel;
+        option.value = valueParkingSpacea[i].pspParkingSpaceId;
+        tempList.add(option);
+      }
     });
   });
 }
+window.onload = dodajParkingSpaces();
 
-// function initMap() {
-//   const uluru = { lat: 42.651375, lng: 18.091252 };
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 15,
-//     center: uluru,
+// var form = document.getElementById("mojaForma");
+// function handleForm(event) {
+//   event.preventDefault();
+// }
+// form.addEventListener("submit", handleForm);
+
+// function prikazi() {
+//   Promise.allSettled([parkingSpaces()]).then((parkirniSpaceovi) => {
+//     $(function () {
+//       console.log(tempList.value);
+//       var park = parkirniSpaceovi[0];
+//       var valueParkingSpacea = park[Object.keys(park)[1]];
+//       var tempList = document.getElementById("parkirniSpaceovi");
+//       for (var i = 0; i < valueParkingSpacea.length; i++) {}
+//     });
 //   });
-//   const plava = "ikonica.png";
-//   const crvena = "crvena-ikona.png";
-//   const zelena = "zelena-ikona.png";
-
-//   Promise.any([userAction()])
-//     .then((markers) => {
-//       for (var i = 0; i < markers.length - 1; i++) {
-//         if (markers[i].sptOccupied == true) {
-//           const marker6 = new google.maps.Marker({
-//             position: {
-//               lat: markers[i].sptLatitude,
-//               lng: markers[i].sptLongitude,
-//             },
-//             map: map,
-//             icon: crvena,
-//           });
-//         } else {
-//           const marker6 = new google.maps.Marker({
-//             position: {
-//               lat: markers[i].sptLatitude,
-//               lng: markers[i].sptLongitude,
-//             },
-//             map: map,
-//             icon: zelena,
-//           });
-//         }
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Failed to fetch");
-//     });
 // }
-
-// const userAction = async () => {
-//   const response = await fetch(
-//     "https://localhost:7236/api/Parking/allParkingSpots",
-//     {
-//       method: "GET",
-//       // body: myBody, // string or object
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-//   const myJson = await response.json(); //extract JSON from the http response
-//   // do something with myJson
-
-//   return myJson;
-// };
-// window.initMap = initMap;
-
-// function initMap2() {
-//   Promise.any([userAction()])
-//     .then((markers) => {
-//       for (var i = 0; i < markers.length - 1; i++) {
-//         const contentString =
-//           '<div id="content">' +
-//           '<div id="siteNotice">' +
-//           "</div>" +
-//           '<h1 id="firstHeading" class="firstHeading"></h1>' +
-//           '<div id="bodyContent">' +
-//           "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-//           "sandstone rock formation in the southern part of the " +
-//           "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-//           "south west of the nearest large town, Alice Springs; 450&#160;km " +
-//           "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-//           "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-//           "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-//           "Aboriginal people of the area. It has many springs, waterholes, " +
-//           "rock caves and ancient paintings. Uluru is listed as a World " +
-//           "Heritage Site.</p>" +
-//           '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-//           "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-//           "(last visited June 22, 2009).</p>" +
-//           "</div>" +
-//           "</div>";
-
-//         const infowindow = new google.maps.InfoWindow({
-//           content: contentString,
-//         });
-
-//         markers[i].addListener("click", () => {
-//           infowindow.open({
-//             anchor: markers[i],
-//             map,
-//             shouldFocus: false,
-//           });
-//         });
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Failed to fetch", error);
-//     });
-// }
-
-// window.initMap2 = initMap2;
